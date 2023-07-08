15a58cadf359d8ab0797576923f8c39d2b029879
Josh Sandquist <joshdsand@gmail.com>
2023-07-08 10:42:11 -0700

    started apollo server setup

--- server/server.js
+++ server/server.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+//import apollo server
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas')
+//const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//add apollo server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+})
+
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -18,4 +30,5 @@ app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`GraphQL server ready at http://localhost:${PORT}${server.graphqlPath}`);
 });
